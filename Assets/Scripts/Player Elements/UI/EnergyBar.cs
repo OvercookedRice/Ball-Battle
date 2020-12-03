0b5d28ccf9d7ad3fe4bac35dbb40d3fe5a1beb72
L1ly <vkthanh1999@gmail.com>
2020-12-03 23:11:10 +0700

    + Added a simple UI prototype. + Added EnergyBar and EnergyCell classes, the EnergyBar will manage all the cells and place them dynamically & neatly.

---
+++ Assets/Scripts/Player Elements/UI/EnergyBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBar : MonoBehaviour
+{
+    [SerializeField] private GameObject cell;
+    private GameObject[] cells;
+
+    void Start()
+    {
+        SetNumEnergy(6);
+    }
+
+    public void SetNumEnergy(int number)
+    {
+        if (cells != null)
+        {
+            foreach(GameObject _ in cells)
+            {
+                Destroy(_);
+            }
+        }
+
+        cells = new GameObject[number];
+
+        float whole_bar_width = GetComponent<RectTransform>().rect.width;
+        float cell_new_width = whole_bar_width / number;
+
+        float first_pos = -whole_bar_width / 2 + cell_new_width / 2; 
+        for (int i = 0; i < number; i++)
+        {
+            cells[i] = Instantiate(cell, transform);
+
+            EnergyCell c = cells[i].GetComponent<EnergyCell>();
+            c.SetWidth(cell_new_width);
+            c.SetAnchoredX(first_pos + cell_new_width * i);
+        }
+    }
+}
---
+++ Assets/Scripts/Player Elements/UI/EnergyCell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyCell : MonoBehaviour
+{
+    [SerializeField] private Image fill;
+
+    public void SetWidth(float width)
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        Vector2 new_size = new Vector2(width, rect.sizeDelta.y);
+
+        rect.sizeDelta = new_size;
+        fill.GetComponent<RectTransform>().sizeDelta = new_size;
+    }
+
+    public void SetAnchoredX(float x)
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(x, rect.anchoredPosition.y);
+    }
+}
